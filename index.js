27a1106c819d002b3c4f45c1d6816ee412849bdb
Martin Micunda <martinmicunda@hotmail.com>
2014-08-06 19:06:35 +0100

    chore(typo): typo fixes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var logger   = require('mm-node-logger')(module),
 /**
  * This module follow best practice for creating, maintaining and using a Mongoose connection like:
  *  - open the connection when the app process start
- *  - start the app server when after the database connection was open (optional)
+ *  - start the app server when after the database connection is open (optional)
  *  - monitor the connection events (`connected`, `error` and `disconnected`)
  *  - close the connection when the app process terminates
  *
